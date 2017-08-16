b540f2163779f1a775dcfabec27dc2166be88797
Franck Ernewein <franck.ernewein@gmail.com>
2017-08-16 17:40:51 +0200

    task(Skills): improve dots line

--- src/SkillBar.jsx
+++ src/SkillBar.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './SkillBar.css';
 
+const lotOfPoints = new Array(80).fill('.').join('');
+
 export default function SkillBar(props) {
-  const {value, label, maxValue=5, maxLabelLength=15} = props;
+  const {value, label, maxValue=5} = props;
   const squares = new Array(maxValue).fill().map((_, i) => {
     const className = 'unit' + (i > (value - 1) ? ' empty' : '');
     return <div key={i} className={className}></div>;
   });
-  const labelString = label + (new Array(maxLabelLength - label.length).fill('.').join(''));
+  const labelString = label + lotOfPoints;
   return <div className="SkillBar">
     <span className="label">{labelString}</span>
     <span className="units">{squares}</span>
