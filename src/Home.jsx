3ea52ff07d3343c796f62dfd12023ff7da42c181
Franck Ernewein <franck.ernewein@gmail.com>
2017-08-21 21:04:02 +0200

    task(Home): wording, add my name

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,6 +7,8 @@ export default function Home(){
     <img src="/img/me.jpg" alt="Me" />
     <h2><TypeWriter timer={90} text="Hello" /></h2>
     <p>
+      My name is Franck Ernewein.<br />
+      <br />
       I'm software engineer, strong javascripter, web interface friendly.<br />
       I'm also a French entrepreneur, founder of <a href="https://www.tweetping.net/">tweetping.net</a><br />
       I'm 34 years old.<br />
